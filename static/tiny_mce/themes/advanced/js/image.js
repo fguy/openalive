bec3c70ac00ba66224a07160d20b8ef4d2009135
taehoon-kang <taehoon.kang@kontagent.com>
2012-02-18 01:09:32 -0800

    Added image upload and video functions.

--- static/tiny_mce/themes/advanced/js/image.js
+++ static/tiny_mce/themes/advanced/js/image.js
@@ -1,251 +1,143 @@
 var ImageDialog = {
-	preInit : function() {
-		var url;
+  preInit : function() {
+    var url;
 
-		tinyMCEPopup.requireLangPack();
+    tinyMCEPopup.requireLangPack();
 
-		if (url = tinyMCEPopup.getParam("external_image_list_url"))
-			document.write('<script language="javascript" type="text/javascript" src="' + tinyMCEPopup.editor.documentBaseURI.toAbsolute(url) + '"></script>');
-	},
+    if (url = tinyMCEPopup.getParam("external_image_list_url"))
+      document.write('<script language="javascript" type="text/javascript" src="' + tinyMCEPopup.editor.documentBaseURI.toAbsolute(url) + '"></script>');
+  },
 
-	init : function() {
-		var f = document.forms[0], ed = tinyMCEPopup.editor;
+  init : function() {
+    var f = document.forms[0], ed = tinyMCEPopup.editor;
 
-		// Setup browse button
-		document.getElementById('srcbrowsercontainer').innerHTML = getBrowserHTML('srcbrowser','src','image','theme_advanced_image');
-		if (isVisible('srcbrowser'))
-			document.getElementById('src').style.width = '180px';
+    // Setup browse button
+    document.getElementById('srcbrowsercontainer').innerHTML = getBrowserHTML('srcbrowser','src','image','theme_advanced_image');
+    if (isVisible('srcbrowser'))
+      document.getElementById('src').style.width = '180px';
 
-		e = ed.selection.getNode();
+    e = ed.selection.getNode();
 
-		this.fillFileList('image_list', tinyMCEPopup.getParam('external_image_list', 'tinyMCEImageList'));
+    this.fillFileList('image_list', 'tinyMCEImageList');
 
-		if (e.nodeName == 'IMG') {
-			f.src.value = ed.dom.getAttrib(e, 'src');
-			f.alt.value = ed.dom.getAttrib(e, 'alt');
-			f.border.value = this.getAttrib(e, 'border');
-			f.vspace.value = this.getAttrib(e, 'vspace');
-			f.hspace.value = this.getAttrib(e, 'hspace');
-			f.width.value = ed.dom.getAttrib(e, 'width');
-			f.height.value = ed.dom.getAttrib(e, 'height');
-			f.insert.value = ed.getLang('update');
-			this.styleVal = ed.dom.getAttrib(e, 'style');
-			selectByValue(f, 'image_list', f.src.value);
-			selectByValue(f, 'align', this.getAttrib(e, 'align'));
-			this.updateStyle();
-		}
-	},
+    if (e.nodeName == 'IMG') {
+      f.src.value = ed.dom.getAttrib(e, 'src');
+      f.insert.value = ed.getLang('update');
+      selectByValue(f, 'image_list', f.src.value);
+    }
+  },
 
-	fillFileList : function(id, l) {
-		var dom = tinyMCEPopup.dom, lst = dom.get(id), v, cl;
+  fillFileList : function(id, l) {
+    var dom = tinyMCEPopup.dom, lst = dom.get(id), v, cl;
 
-		l = typeof(l) === 'function' ? l() : window[l];
+    l = window[l];
 
-		if (l && l.length > 0) {
-			lst.options[lst.options.length] = new Option('', '');
+    if (l && l.length > 0) {
+      lst.options[lst.options.length] = new Option('', '');
 
-			tinymce.each(l, function(o) {
-				lst.options[lst.options.length] = new Option(o[0], o[1]);
-			});
-		} else
-			dom.remove(dom.getParent(id, 'tr'));
-	},
+      tinymce.each(l, function(o) {
+        lst.options[lst.options.length] = new Option(o[0], o[1]);
+      });
+    } else
+      dom.remove(dom.getParent(id, 'tr'));
+  },
 
-	update : function() {
-		var f = document.forms[0], nl = f.elements, ed = tinyMCEPopup.editor, args = {}, el;
+  update : function() {
+    var f = document.forms[0], nl = f.elements, ed = tinyMCEPopup.editor, args = {}, el;
 
-		tinyMCEPopup.restoreSelection();
+    tinyMCEPopup.restoreSelection();
 
-		if (f.src.value === '') {
-			if (ed.selection.getNode().nodeName == 'IMG') {
-				ed.dom.remove(ed.selection.getNode());
-				ed.execCommand('mceRepaint');
-			}
+    if (f.src.value === '') {
+      if (ed.selection.getNode().nodeName == 'IMG') {
+        ed.dom.remove(ed.selection.getNode());
+        ed.execCommand('mceRepaint');
+      }
 
-			tinyMCEPopup.close();
-			return;
-		}
+      tinyMCEPopup.close();
+      return;
+    }
 
-		if (!ed.settings.inline_styles) {
-			args = tinymce.extend(args, {
-				vspace : nl.vspace.value,
-				hspace : nl.hspace.value,
-				border : nl.border.value,
-				align : getSelectValue(f, 'align')
-			});
-		} else
-			args.style = this.styleVal;
+    tinymce.extend(args, {
+      src : f.src.value,
+    });
 
-		tinymce.extend(args, {
-			src : f.src.value.replace(/ /g, '%20'),
-			alt : f.alt.value,
-			width : f.width.value,
-			height : f.height.value
-		});
+    el = ed.selection.getNode();
 
-		el = ed.selection.getNode();
+    if (el && el.nodeName == 'IMG') {
+      ed.dom.setAttribs(el, args);
+    } else {
+      ed.execCommand('mceInsertContent', false, '<img id="__mce_tmp" />', {skip_undo : 1});
+      ed.dom.setAttribs('__mce_tmp', args);
+      ed.dom.setAttrib('__mce_tmp', 'id', '');
+      ed.undoManager.add();
+    }
 
-		if (el && el.nodeName == 'IMG') {
-			ed.dom.setAttribs(el, args);
-			tinyMCEPopup.editor.execCommand('mceRepaint');
-			tinyMCEPopup.editor.focus();
-		} else {
-			tinymce.each(args, function(value, name) {
-				if (value === "") {
-					delete args[name];
-				}
-			});
+    tinyMCEPopup.close();
+  },
 
-			ed.execCommand('mceInsertContent', false, tinyMCEPopup.editor.dom.createHTML('img', args), {skip_undo : 1});
-			ed.undoManager.add();
-		}
+  getImageData : function() {
+    var f = document.forms[0];
 
-		tinyMCEPopup.close();
-	},
-
-	updateStyle : function() {
-		var dom = tinyMCEPopup.dom, st, v, f = document.forms[0];
-
-		if (tinyMCEPopup.editor.settings.inline_styles) {
-			st = tinyMCEPopup.dom.parseStyle(this.styleVal);
-
-			// Handle align
-			v = getSelectValue(f, 'align');
-			if (v) {
-				if (v == 'left' || v == 'right') {
-					st['float'] = v;
-					delete st['vertical-align'];
-				} else {
-					st['vertical-align'] = v;
-					delete st['float'];
-				}
-			} else {
-				delete st['float'];
-				delete st['vertical-align'];
-			}
-
-			// Handle border
-			v = f.border.value;
-			if (v || v == '0') {
-				if (v == '0')
-					st['border'] = '0';
-				else
-					st['border'] = v + 'px solid black';
-			} else
-				delete st['border'];
-
-			// Handle hspace
-			v = f.hspace.value;
-			if (v) {
-				delete st['margin'];
-				st['margin-left'] = v + 'px';
-				st['margin-right'] = v + 'px';
-			} else {
-				delete st['margin-left'];
-				delete st['margin-right'];
-			}
-
-			// Handle vspace
-			v = f.vspace.value;
-			if (v) {
-				delete st['margin'];
-				st['margin-top'] = v + 'px';
-				st['margin-bottom'] = v + 'px';
-			} else {
-				delete st['margin-top'];
-				delete st['margin-bottom'];
-			}
-
-			// Merge
-			st = tinyMCEPopup.dom.parseStyle(dom.serializeStyle(st), 'img');
-			this.styleVal = dom.serializeStyle(st, 'img');
-		}
-	},
-
-	getAttrib : function(e, at) {
-		var ed = tinyMCEPopup.editor, dom = ed.dom, v, v2;
-
-		if (ed.settings.inline_styles) {
-			switch (at) {
-				case 'align':
-					if (v = dom.getStyle(e, 'float'))
-						return v;
-
-					if (v = dom.getStyle(e, 'vertical-align'))
-						return v;
-
-					break;
-
-				case 'hspace':
-					v = dom.getStyle(e, 'margin-left')
-					v2 = dom.getStyle(e, 'margin-right');
-					if (v && v == v2)
-						return parseInt(v.replace(/[^0-9]/g, ''));
-
-					break;
-
-				case 'vspace':
-					v = dom.getStyle(e, 'margin-top')
-					v2 = dom.getStyle(e, 'margin-bottom');
-					if (v && v == v2)
-						return parseInt(v.replace(/[^0-9]/g, ''));
-
-					break;
-
-				case 'border':
-					v = 0;
-
-					tinymce.each(['top', 'right', 'bottom', 'left'], function(sv) {
-						sv = dom.getStyle(e, 'border-' + sv + '-width');
-
-						// False or not the same as prev
-						if (!sv || (sv != v && v !== 0)) {
-							v = 0;
-							return false;
-						}
-
-						if (sv)
-							v = sv;
-					});
-
-					if (v)
-						return parseInt(v.replace(/[^0-9]/g, ''));
-
-					break;
-			}
-		}
-
-		if (v = dom.getAttrib(e, at))
-			return v;
-
-		return '';
-	},
-
-	resetImageData : function() {
-		var f = document.forms[0];
-
-		f.width.value = f.height.value = "";	
-	},
-
-	updateImageData : function() {
-		var f = document.forms[0], t = ImageDialog;
-
-		if (f.width.value == "")
-			f.width.value = t.preloadImg.width;
-
-		if (f.height.value == "")
-			f.height.value = t.preloadImg.height;
-	},
-
-	getImageData : function() {
-		var f = document.forms[0];
-
-		this.preloadImg = new Image();
-		this.preloadImg.onload = this.updateImageData;
-		this.preloadImg.onerror = this.resetImageData;
-		this.preloadImg.src = tinyMCEPopup.editor.documentBaseURI.toAbsolute(f.src.value);
-	}
+    this.preloadImg = new Image();
+    this.preloadImg.src = tinyMCEPopup.editor.documentBaseURI.toAbsolute(f.src.value);
+  }
 };
+
+var ImageUpload = { 
+  uri : 'http://files.nicedit.com/',
+  requestInterval : false,
+  id : 0,
+  
+  init: function() {
+    ImageUpload.requestInterval = 1000;
+    ImageUpload.id = Math.round(Math.random()*Math.pow(10,15));
+  },
+
+  startUpload : function() {
+    var myDoc = document.getElementById("uploadFrame").contentWindow.document;
+    myDoc.uploadForm.action = ImageUpload.uri + '?id=' + ImageUpload.id;
+    myDoc.uploadForm.APC_UPLOAD_PROGRESS.value = ImageUpload.id;
+    myDoc.uploadForm.style.display = 'none';
+    myDoc.getElementById("status").innerHTML = '<img src="http://files.nicedit.com/ajax-loader.gif" style="float: right; margin-right: 40px;" /><strong>Uploading...</strong><br />Please wait';
+    myDoc.uploadForm.submit();
+    setTimeout(ImageUpload.makeRequest,ImageUpload.requestInterval);
+  },
+  
+  makeRequest : function() {
+    var s = document.createElement("SCRIPT");
+    s.type = 'text/javascript';
+    s.src = ImageUpload.uri + '?check=' + ImageUpload.id + '&rand=' + +Math.round(Math.random()*Math.pow(10,15)); 
+    s.onload = function() {
+      s.parentNode.removeChild(s);
+    }
+    document.getElementsByTagName('head')[0].appendChild(s);
+    if(ImageUpload.requestInterval) {
+      setTimeout(ImageUpload.makeRequest, ImageUpload.requestInterval);
+    }
+  }
+};
+
+var nicUploadButton = {
+  statusCb : function(o) {
+    if(o.url) {
+      ImageUpload.requestInterval = false;
+      document.forms[0].src.value = o.url;
+      ImageDialog.update();
+    } else if(o.error) {
+      alert("There was an error uploading your image ("+o.error+").");
+      tinyMCEPopup.close();
+    } else if(o.noprogress) {
+      if(ImageUpload.uri.indexOf('http:') == -1 || ImageUpload.uri.indexOf(window.location.host) != -1) {
+        ImageUpload.requestInterval = false;
+      }
+    } 
+  }
+};
+//
+//nicUploadButton.statusCb = function(o) {
+//  nicUploadButton.lastPlugin.update(o);
+//}
 
 ImageDialog.preInit();
 tinyMCEPopup.onInit.add(ImageDialog.init, ImageDialog);
+tinyMCEPopup.onInit.add(ImageUpload.init, ImageUpload);
---
+++ static/tiny_mce/themes/advanced/js/video.js
@@ -0,0 +1,101 @@
+var VideoDialog = {
+  preInit : function() {
+    var url;
+
+    tinyMCEPopup.requireLangPack();
+
+    if (url = tinyMCEPopup.getParam("external_video_list_url"))
+      document.write('<script language="javascript" type="text/javascript" src="' + tinyMCEPopup.editor.documentBaseURI.toAbsolute(url) + '"></script>');
+  },
+
+  init : function() {
+    var f = document.forms[0], ed = tinyMCEPopup.editor;
+
+    // Setup browse button
+    document.getElementById('srcbrowsercontainer').innerHTML = getBrowserHTML('srcbrowser','src','video','theme_advanced_video');
+    if (isVisible('srcbrowser'))
+      document.getElementById('src').style.width = '180px';
+
+    e = ed.selection.getNode();
+
+    this.fillFileList('video_list', 'tinyMCEVideoList');
+
+    if (e.nodeName == 'IFRAME') {
+      var src = ed.dom.getAttrib(e, 'src');
+      if(src.indexof("http://www.youtube.com/embed/") === 0) {
+        f.src.value = src;
+        f.insert.value = ed.getLang('update');
+        selectByValue(f, 'video_list', f.src.value);        
+      }
+    }
+  },
+
+  fillFileList : function(id, l) {
+    var dom = tinyMCEPopup.dom, lst = dom.get(id), v, cl;
+
+    l = window[l];
+
+    if (l && l.length > 0) {
+      lst.options[lst.options.length] = new Option('', '');
+
+      tinymce.each(l, function(o) {
+        lst.options[lst.options.length] = new Option(o[0], o[1]);
+      });
+    } else
+      dom.remove(dom.getParent(id, 'tr'));
+  },
+
+  update : function() {
+    var f = document.forms[0], nl = f.elements, ed = tinyMCEPopup.editor, args = {}, el;
+
+    tinyMCEPopup.restoreSelection();
+
+    if (f.src.value === '') {
+      if (ed.selection.getNode().nodeName == 'IFRAME') {
+        ed.dom.remove(ed.selection.getNode());
+        ed.execCommand('mceRepaint');
+      }
+
+      tinyMCEPopup.close();
+      return;
+    }
+
+    el = ed.selection.getNode();
+    
+    if (el && el.nodeName == 'IFRAME') {
+      ed.dom.setAttribs(el, args);
+    } else {
+      var src = this.parseV(f.src.value);
+      if(src) {
+        ed.execCommand('mceInsertContent', false, '<iframe type="text/html" frameborder="0" src="' + src + '" width="450" height="338"></iframe>', {skip_undo : 1});
+  //      ed.dom.setAttribs('__mce_tmp', args);
+        //ed.dom.setAttrib('__mce_tmp', 'id', '');
+        ed.undoManager.add();
+      }
+    }
+
+    tinyMCEPopup.close();
+  },
+
+  parseV : function(v) {
+    if(v.indexOf("http://www.youtube.com/") !== 0) {
+      tinyMCEPopup.alert(tinyMCEPopup.editor.getLang("advanced_dlg.video_not_support"));
+      tinyMCEPopup.close();
+      return;
+    }
+    // YouTube
+
+    if (v.match(/watch\?v=(.*)/)) {
+      return 'http://www.youtube.com/embed/' + v.match(/v=(.*)/)[0].split('=')[1].split("&")[0];
+    } else if(v.match(/v\/(.*)/)) {
+      return 'http://www.youtube.com/embed/' + v.match(/v\/(.*)/)[0].split('=')[1].split("&")[0];
+    }
+    return v;
+  },
+  
+  getVideoData : function() {
+  }
+};
+
+VideoDialog.preInit();
+tinyMCEPopup.onInit.add(VideoDialog.init, VideoDialog);
